3bbc8e0d50d372c013689a3abe56e274c68ccc9f
Adriano Dantas <adrianodts@msn.com>
2021-04-08 19:12:09 -0300

    Criando recurso Genre

--- database/seeds/GenresTableSeeder.php
+++ database/seeds/GenresTableSeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 
-class CategoriesSeeder extends Seeder
+class GenresTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,6 +11,6 @@ class CategoriesSeeder extends Seeder
      */
     public function run()
     {
-        factory(\App\Models\Category::class, 100)->create();
+        factory(\App\Models\Genre::class, 100)->create();
     }
 }
