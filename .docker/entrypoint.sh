323081d6165e06af9453b923cca701a7754dd4ae
Adriano Dantas <adrianodts@msn.com>
2021-04-16 22:03:16 -0300

    Implementacao dos testes com HTTP

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
-#cp .env.example .env
-#cp .env.testing.example .env.testing
-entrypoint: dockerize -template ./.docker/app/.env:.env -template ./.docker/app/.env.testing:.env.testing -wait tcp://db:3306 -timeout 40s ./.docker/entrypoint.sh
+cp .env.example .env
+cp .env.testing.example .env.testing
 #chown -R www-data:www-data .
 composer install
 php artisan key:generate
